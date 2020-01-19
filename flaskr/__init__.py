e65758d164fc755d29662af0c9ff3096ca45fe8a
Jerry Trymander <jert47@yahoo.com>
2020-01-19 17:15:06 +0100

    The one and only flaskr commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,10 @@
 import os
-
 from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    print(app.instance_path)
+    print('app.instance_path', app.instance_path)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -28,5 +27,16 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    # apply the blueprints to the app
+    from flaskr import auth, blog
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+
+    app.add_url_rule("/", endpoint="index")
 
     return app
--- flaskr/db.py
+++ flaskr/db.py
@@ -1,20 +1,46 @@
-import sqlite3
-import click
+import sqlite3, click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
+        # g - a special object, unique for each request. It stores 
+        # data (eg. the connection) accessed by multiple functions 
+        # during a request, eg get_db().
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-        g.db.row_factory = sqlite3.Row
-
+        g.db.row_factory = sqlite3.Row # tells the connection to 
+                                       # return rows that behave
+                                       # like dicts
     return g.db
 
-def close_db(e=None):
-    db = g.pop('db', None)
+def close_db(e=None):       # check if g.db was set (meaning the
+    db = g.pop('db', None)  # connection was created) and if so, 
+                            # close it.
 
     if db is not None:
         db.close()
+
+def init_db():
+    """Clear existing data and create new tables."""
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    # Clear the existing data and create new tables.
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    """Register database functions with the Flask app. This is called by
+    the application factory.
+    """
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
